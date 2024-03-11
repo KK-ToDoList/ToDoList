e0b15c58b86a12b80297cb3eb9946b0161ce95eb
BONHOOW <gkh30878@gmail.com>
2024-03-11 14:05:54 +0900

    화면구성, DB연결

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 import axios from "axios";
 
@@ -6,23 +6,82 @@ function App() {
 
   const baseUrl = "http://localhost:8080"     //baseUrl상수 선언
 
-  useEffect(() => {     //react앱을 실행시 1번만 실행
+  const [todos, setTodos] = useState([]);     //입력값을 받아 todos에 입력
+  const [input, setInput] = useState("");
+
+  //react앱을 실행시 1번만 실행
+  useEffect(() => {   
     getTodos();
   }, []); 
 
-    async function getTodos(){      //getTodos메소드 불러오기
+  //getTodos메소드 불러오기
+    async function getTodos(){ 
       await axios
         .get(baseUrl + "/todo")
         .then((response) => {
-          console.log(response.data)
+          console.log(response.data);   
+          setTodos(response.data)   //setTodos에 데이터 입력
         })
         .catch((error) => {
           console.error(error)
         })
     }
-  return (
-    <div className="App">
-      hello
+    
+    function insertTodo(e){
+      e.preventDefault();
+
+      const insertTodo = async () => {
+        await axios
+              .post(baseUrl + "/todo", {
+                todoName: input
+              })
+              .then((response) =>{
+                console.log (response.data)
+                setInput("");
+                getTodos();
+              })
+              .catch((error) => {
+                console.error(error);
+              })
+      }
+      insertTodo();
+      console.log("할일이 추가됨!")
+    }
+
+    //changeText 함수 생성
+    function changeText(e){
+      e.preventDefault();
+      setInput(e.target.value);
+      console.log("입력 값 = " + input);
+    }
+
+    //화면 구성
+    return (
+    <div className="App">  
+      <h1>TODO LIST</h1>
+      <form onSubmit={insertTodo}>
+        <label>
+        Todo &nbsp;
+        <input type = "text" required = {true} value = {input} onChange = {changeText}/>
+        </label>  
+        <input type = "submit" value = "Create"/> {/*Create BTN*/}         
+      </form>
+      {
+        todos
+        ? todos.map((todo) => {
+          return(
+            <div className="todo" key={todo.id}>
+              <h3>
+                <label
+                  onClick={null}>
+                  {todo.todoName}         {/* 각 todo의 이름을 출력 */}
+                </label>
+              </h3>
+            </div>
+          )
+        })
+        :null
+      }
     </div>
   );
 }
