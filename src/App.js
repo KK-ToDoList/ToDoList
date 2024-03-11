f7a0150d7de9695f1a346256dc2f2131d95bfe6b
BONHOOW <gkh30878@gmail.com>
2024-03-11 12:18:06 +0900

    axios 설치, getTodos메소드 연결

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React, { useEffect } from "react";
 import './App.css';
+import axios from "axios";
 
 function App() {
+
+  const baseUrl = "http://localhost:8080"     //baseUrl상수 선언
+
+  useEffect(() => {     //react앱을 실행시 1번만 실행
+    getTodos();
+  }, []); 
+
+    async function getTodos(){      //getTodos메소드 불러오기
+      await axios
+        .get(baseUrl + "/todo")
+        .then((response) => {
+          console.log(response.data)
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello
     </div>
   );
 }
